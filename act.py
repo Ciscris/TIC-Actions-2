853864b251f8c84b686cf97612a3a156c07689bd
MIMOAC <mimoac8@gmail.com>
2024-04-16 14:01:53 -0600

    programa de python

---
+++ act.py
@@ -0,0 +1,10 @@
+# Definimos los dos números enteros
+num1 = 5
+num2 = 7
+
+# Iniciamos el ciclo for
+for i in range(3):
+    # Realizamos la suma
+    suma = num1 + num2
+    # Imprimimos el resultado en la consola
+    print(f"La suma de {num1} y {num2} es: {suma}")
